181b2d962a4d6e716ec6b846920efe69e3edf192
philc <philc@arcx.com>
2020-01-23 20:18:09 -0500

    added workout details preview
    Change-Id: I8634952ed83f19b18139b24c9d5f390c73c3536c

--- __init__.py
+++ __init__.py
@@ -107,13 +107,13 @@ class C25kSkill(MycroftSkill):
             for interval_type in each_interval:
                 workout_duration = workout_duration + each_interval[interval_type]
         workout_duration = int(workout_duration / 60)  # minutes
-        wait_while_speaking()
-        self.speak_dialog('details_001', data={"week": this_week["Name"], "day": this_day["Name"]},
-                          expect_response=False)
-        self.speak_dialog('details_002', data={"duration": str(workout_duration)},
-                          expect_response=False)
-        self.speak_dialog('details_003', data={"intervals": str(interval_count)},
-                          expect_response=False)
+        # wait_while_speaking()
+        #self.speak_dialog('details_001', data={"week": this_week["Name"], "day": this_day["Name"]},
+        #                  expect_response=False)
+        #self.speak_dialog('details_002', data={"duration": str(workout_duration)},
+        #                  expect_response=False)
+        #self.speak_dialog('details_003', data={"intervals": str(interval_count)},
+        #                  expect_response=False)
         interval_list = enumerate(all_intervals)
         try:
             for index, value in interval_list:
